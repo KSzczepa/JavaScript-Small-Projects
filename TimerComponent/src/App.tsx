6c965d4363971c6f1945e64ee43df7b5033d8cdc
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2022-12-04 21:56:25 +0100

    Update App.tsx
    Cosmetics Changes

--- TimerComponent/src/App.tsx
+++ TimerComponent/src/App.tsx
@@ -1,24 +1,26 @@
-import Timer from "./components/Timer.tsx";
+import Timer from "./components/LastSeenTimer.tsx";
 import React, { useState } from "react";
 
 function App() {
 
-	const [number, setNumber] = React.useState(0);
+	const [enteredNumber, setEnteredNumber] = React.useState(0);
 	const [lastSeen, setLastSeen] = useState(0);
 
 	const setNumberHandler = (val) => {
-		setNumber(val);
+		setEnteredNumber(val);
 	}
 
 	const submitHandler = () => {
-		setLastSeen(number);
+		setLastSeen(enteredNumber);
 	}
 
 	return (
 		<div className="app">
-			<input type='number' onInput={e => setNumberHandler(e.target.value)} min='0'></input>
-			<button onClick={submitHandler} value='Submit'>Click</button>
-			<Timer enteredTime={Number(lastSeen)} />
+			<h2>The current epoch unix timestamp</h2>
+			<span>Enter a timestamp:    </span>
+			<input type='number' onInput={e => setNumberHandler(e.target.value)}></input>
+			<button onClick={submitHandler} value='Submit'>Check</button>
+			<Timer enteredData={Number(lastSeen)} />
 		</div>
 	);
 }
